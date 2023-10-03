5447f86a454a30b2ac6970d8471a7d02756b2c84
Augusto Musetti <musettiaugusto@gmail.com>
2023-10-03 13:52:13 -0300

    Neotree,sniprun and catppuccin

---
+++ lua/augusto/plugins/file-explorer.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    keys = {
+      { "<leader>fe", "<cmd>Neotree dir=%:p:h position=current<cr>", desc = "File Explorer" },
+      { "<leader>fl", "<cmd>Neotree dir=%:p:h position=left<cr>",    desc = "File Explorer" },
+    },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
+      end
+    end,
+    opts = {
+      popup_border_style = "rounded",
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        position = "current",
+        mappings = {
+          ["<space>"] = "none",
+          ["Y"] = function(state)
+            local node = state.tree:get_node()
+            local content = node.path
+            -- relative
+            -- local content = node.path:gsub(state.path, ""):sub(2)
+            vim.fn.setreg('"', content)
+            vim.fn.setreg("1", content)
+            vim.fn.setreg("+", content)
+          end,
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    },
+    cond = function()
+      return not vim.g.vscode
+    end,
+  },
+}
---
+++ lua/augusto/plugins/terminal.lua
@@ -0,0 +1,146 @@
+return {
+	-- {
+	-- 	"akinsho/toggleterm.nvim",
+	-- 	enabled = true,
+	-- 	cond = function()
+	-- 		return not vim.g.vscode
+	-- 	end,
+	-- 	config = function()
+	-- 		require("toggleterm").setup({
+	-- 			shade_terminals = false,
+	-- 			hide_numbers = true, -- hide the number column in toggleterm buffers
+	-- 			autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+	-- 			start_in_insert = true,
+	-- 			insert_mappings = true, -- whether or not the open mapping applies in insert mode
+	-- 			terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+	-- 			persist_size = true,
+	-- 			persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+	-- 			direction = "float",
+	-- 			close_on_exit = true, -- close the terminal window when the process exits
+	-- 			shell = "/bin/fish",
+	-- 			auto_scroll = true, -- automatically scroll to the bottom on terminal output
+	-- 			-- This field is only relevant if direction is set to 'float'
+	-- 			float_opts = {
+	-- 				border = "rounded",
+	-- 			},
+	-- 			-- like `size`, width and height can be a number or function which is passed the current terminal
+	-- 			winbar = {
+	-- 				enabled = false,
+	-- 			},
+	-- 			-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+	-- 			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()"),
+	-- 		})
+	--
+	-- 		-- Send the contents of the current buffer to a terminal buffer in neovim.
+	-- 		--
+	-- 		-- Args:
+	-- 		--   term_bufnr: The buffer number of the terminal buffer to send the contents to.
+	-- 		--
+	-- 		local terminal = require("toggleterm")
+	-- 		local ft_cmds = {
+	-- 			python = "python3 " .. vim.fn.expand("%"),
+	-- 			matlab = "octave " .. vim.fn.expand("%"),
+	-- 		}
+	-- 		function _G.set_terminal_keymaps()
+	-- 			local opts = { buffer = 0 }
+	-- 			vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	-- 			vim.keymap.set("t", "<C-w>h", [[<Cmd>wincmd h<CR>]], opts)
+	-- 			vim.keymap.set("t", "<C-w>j", [[<Cmd>wincmd j<CR>]], opts)
+	-- 			vim.keymap.set("t", "<C-w>k", [[<Cmd>wincmd k<CR>]], opts)
+	-- 			vim.keymap.set("t", "<C-w>l", [[<Cmd>wincmd l<CR>]], opts)
+	-- 		end
+	--
+	-- 		vim.keymap.set({ "n", "i", "t" }, "<C-.>", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
+	-- 		vim.keymap.set({ "n", "i", "t" }, "<C-,>", function()
+	-- 			terminal.exec(ft_cmds[vim.bo.filetype])
+	-- 		end, { noremap = true, silent = true })
+	-- 	end,
+	-- 	keys = {
+	-- 		{ "<C-.>", desc = "Toggle Terminal" },
+	-- 		{ "<C-,>", desc = "Execute Buffer" },
+	-- 	},
+	-- },
+	{
+		"michaelb/sniprun",
+		build = "sh ./install.sh",
+		config = function()
+			require("sniprun").setup({
+				selected_interpreters = { "Python3_fifo" },
+				repl_enable = { "Python3_fifo" },
+				repl_disable = {}, --# disable REPL-like behavior for the given interpreters
+
+				interpreter_options = { --# interpreter-specific options, see doc / :SnipInfo <name>
+
+					--# use the interpreter name as key
+					GFM_original = {
+						use_on_filetypes = { "markdown.pandoc" }, --# the 'use_on_filetypes' configuration key is
+						--# available for every interpreter
+					},
+					Python3_original = {
+						error_truncate = "auto", --# Truncate runtime errors 'long', 'short' or 'auto'
+						--# the hint is available for every interpreter
+						--# but may not be always respected
+					},
+				},
+
+				--# you can combo different display modes as desired and with the 'Ok' or 'Err' suffix
+				--# to filter only sucessful runs (or errored-out runs respectively)
+				display = {
+					"Classic", --# display results in the command-line  area
+					"VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
+
+					-- "VirtualText",             --# display results as virtual text
+					-- "TempFloatingWindow",      --# display results in a floating window
+					-- "LongTempFloatingWindow",  --# same as above, but only long results. To use with VirtualText[Ok/Err]
+					-- "Terminal", --# display results in a vertical split
+					-- "TerminalWithCode",        --# display results and code history in a vertical split
+					-- "NvimNotify",              --# display with the nvim-notify plugin
+					-- "Api"                      --# return output to a programming interface
+				},
+
+				live_display = { "VirtualTextOk" }, --# display mode used in live_mode
+
+				display_options = {
+					terminal_scrollback = vim.o.scrollback, --# change terminal display scrollback lines
+					terminal_line_number = false, --# whether show line number in terminal window
+					terminal_signcolumn = false, --# whether show signcolumn in terminal window
+					terminal_persistence = true, --# always keep the terminal open (true) or close it at every occasion (false)
+					terminal_width = 45, --# change the terminal display option width
+					notification_timeout = 5, --# timeout for nvim_notify output
+				},
+
+				--# You can use the same keys to customize whether a sniprun producing
+				--# no output should display nothing or '(no output)'
+				show_no_output = {
+					"Classic",
+					"TempFloatingWindow", --# implies LongTempFloatingWindow, which has no effect on its own
+				},
+
+				--# customize highlight groups (setting this overrides colorscheme)
+
+				live_mode_toggle = "off", --# live mode toggle, see Usage - Running for more info
+
+				--# miscellaneous compatibility/adjustement settings
+				inline_messages = false, --# boolean toggle for a one-line way to display messages
+				--# to workaround sniprun not being able to display anything
+
+				borders = "none", --# display borders around floating windows
+				--# possible values are 'none', 'single', 'double', or 'shadow'
+			})
+
+			vim.keymap.set("v", ",", "<Plug>SnipRun", { silent = true })
+			vim.keymap.set("n", "<leader>,", "<Plug>SnipRunOperator", { silent = true })
+			vim.keymap.set(
+				"n",
+				"<leader>,,",
+				":let b:caret=winsaveview() <CR> | :%SnipRun <CR>| :call winrestview(b:caret) <CR>",
+				{ silent = true }
+			)
+		end,
+		keys = {
+			{ mode = "v", ",", desc = "Run block" },
+			{ mode = "n", "<leader>,", desc = "Run Operator" },
+			{ mode = "n", "<leader>,,", desc = "Run file" },
+		},
+	},
+}
